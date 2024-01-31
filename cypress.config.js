c468c0ff87cf831819b6a0aec37807ad5ab26fbe
Santiago Agost <santiagoagost@gmail.com>
2024-01-31 18:59:22 -0300

    0009

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  video: false,
-  videoCompression: false,
+  video: true,
+  videoCompression: true,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
